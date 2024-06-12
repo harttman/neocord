8bc2fcdae5fc29f663c68dca6242d369c873e92f
harttman <dakiv.offical.vitaliy@gmail.com>
2024-06-12 03:41:54 +0200

    publish

--- index.php
+++ index.php
@@ -6,11 +6,12 @@ use Discord\Discord;
 
 $intents = 513;
 
-$client = new Discord('MTI0OTEyMDM2NjQyOTg2NDA1OQ.G3IC8T.ks2xQM91LM_HEpO9vSL3cLccIXgqiHSeG9mNEM', $intents);
+$client = new Discord('token', $intents);
 
 $client->on('MESSAGE_CREATE', function($msg) use ($client) {
-    echo "Message from {$msg->author}: {$msg->content}" . PHP_EOL;
-    if($msg->content == "hello") {
+    if($msg->author->bot) return;
+    
+    if($msg->content == "!ping") {
       $client->replyToMessage($msg->channel_id, $msg->id, "Hello, {$msg->author->username}!");
     }
 });
